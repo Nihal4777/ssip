22a266bc99a1c5198dcb051a0b3dba7053f2248a
Nihal Rajpal <suryakant.rajpal@gmail.com>
2023-11-04 16:53:00 +0530

    Commit

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -32,7 +32,7 @@ class StocksController extends Controller
             $stock=Stock::find($id);
             $c=Current::where(['item_name'=>$stock->item_name,'center_id'=>$stock->center_id])->count();
             if($c){
-                DB::table('currents')->where(['item_name'=>$stock->item_name,'center_id',$stock->center_id])->increment('qnt', $stock->qnt);    
+                DB::table('currents')->where(['item_name'=>$stock->item_name,'center_id'=>$stock->center_id])->increment('qnt', $stock->qnt);    
             }
             else{
                 $current=new Current;
