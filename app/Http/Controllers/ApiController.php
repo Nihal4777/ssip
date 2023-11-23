b29309bead3f42d716c559eab0689803f92f8a28
Nihal Rajpal <suryakant.rajpal@gmail.com>
2023-11-23 16:12:06 +0530

    Test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -22,8 +22,8 @@ class ApiController extends Controller
         $date=$request->date;
         if(empty($date))
             $date=date('Y-m-d',strtotime('yesterday'));
-        $consumptions=DB::table('consumptions as g')->where(['center_id'=>auth()->user()->center_id,'date'=>$date])->join('items as i','g.item_id','i.id')->join('categories as c','category_id','c.id')->get(['g.id as id','i.name as itemName','c.name as cname','g.created_at as created_at','qnt']);
-        $cds=ConsumptionDocument::where(['center_id'=>auth()->user()->center_id,'date'=>$date])->get();
+        $consumptions=DB::table('consumptions as g')->where(['center_id'=>$request->center_id,'date'=>$date])->join('items as i','g.item_id','i.id')->join('categories as c','category_id','c.id')->get(['g.id as id','i.name as itemName','c.name as cname','g.created_at as created_at','qnt']);
+        $cds=ConsumptionDocument::where(['center_id'=>$request->center_id,'date'=>$date])->get();
         return response()->json(['status'=>'success','message'=>'Consumption details entered successfully','cons_data'=>$consumptions,'docs_data'=> $cds]);
     }
     public function consumption_store(Request $request)
@@ -35,7 +35,7 @@ class ApiController extends Controller
             {
                 $cons=new Consumption(['item_id'=>$item,'qnt'=>$request->qnt[$i],'center_id'=>$user->center_id,'date'=>now()]);
                 // $cons->save();
-                $stock=Stock::where(['item_id'=>$item,'center_id'=>$user->center_id,])->first();
+                $stock=Stock::where(['item_id'=>$item,'center_id'=>$request->center_id,])->first();
                 $stock->qnt-=$request->qnt[$i];
                 // $stock->save();
             }
